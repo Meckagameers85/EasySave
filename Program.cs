dbb0e96a0e58c72737216172e54a91102c211bcf
Mahery RAJAONOSOA <mahery.rajaonosoa@viacesi.fr>
2025-05-12 00:13:48 +0200

    mise en place de la gestion de langue

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    private static ILocalizedResources? _resources;
+
+    static void Main()
+    {
+        
+        Console.Write("Choisissez une langue | Choose a langage (en/fr) : ");
+        var lang = Console.ReadLine();
+
+        _resources = LocalizedResourcesFactory.Create(lang ?? "en");
+    }
+}
